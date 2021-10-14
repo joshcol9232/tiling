e83c0782ae1ab0ebe9acdb2059d37a668d344706
Joshua Colclough <colcloughj2@cardiff.ac.uk>
2021-10-14 16:36:49 +0100

    Vertical line was not drawing properly

--- tests.py
+++ tests.py
@@ -14,17 +14,17 @@ def draw_construction_line(xspace, j, k, sigma, set_angles):
     """
     Function of the k'th grid line of set j, with normal offset sigma. 
     """
-    # Special cases. E.g angle = 0
-    if set_angles[j][0] == 0:
+    # Special cases. E.g angle = 0 or 90 degrees
+    if set_angles[j][1] == 0 or set_angles[j][2] == 0:
         plt.axvline(x=( (k - sigma)/set_angles[j][2] ))
     else:
         plt.plot(xspace, (k - sigma - xspace * set_angles[j][2])/set_angles[j][1])
 
-xspace = np.linspace(-7, 7)
+xspace = np.linspace(-3, 3)
 
 for j in range(5):
-    # for k in range(10):
-    draw_construction_line(xspace, j, 1, 0.1, set_angles)
+    for k in range(3):
+        draw_construction_line(xspace, j, k, 1.2, set_angles)
 
 
 
