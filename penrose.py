eae4b49920d85855d7c170773db620d597bfb0d1
Josh Colclough <colcloughj2@cardiff.ac.uk>
2021-10-16 19:00:54 +0100

    Trying to build a conway worm

--- penrose.py
+++ penrose.py
@@ -3,74 +3,65 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
 from matplotlib.collections import PatchCollection
 
-SIDE_LENGTH = 1
+class Intersection:
+    """
+    Takes r vector position, and the j and k values of the lines that intersect each other
+    """
+    def __init__(self, j1, k1, j2, k2, sigma1, sigma2, symmetry=5):  # Init calculates the intersection from given params
+        self.r = find_intersection(j1, k1, j2, k2, sigma1, sigma2, symmetry=symmetry)
+        self.j1 = j1
+        self.j2 = j2
+        self.k1 = k1
+        self.k2 = k2
+        self.sigma1 = sigma1
+        self.sigma2 = sigma2
+
+    def __repr__(self):
+        return "%s" % self.r
+
+    def find_intersection(j1, k1, j2, k2, sigma1, sigma2, symmetry=5):
+        """
+        Returns position vector of intersection between line j1,k1 and j2,k2
+        """
+        a1 = j1 * 2.0 * np.pi/symmetry
+        a2 = j2 * 2.0 * np.pi/symmetry
+        x = ( (k1 - sigma1)/np.cos(a1) ) - ( (k2 - sigma2)/np.sin(a2) )
+        return np.array([x, construction_line(x, j1, k1, sigma1, symmetry=5)])
+
+
+def construction_line(x, j, k, sigma, symmetry=5):
+    angle = j * 2.0 * np.pi/symmetry
+    return (k - sigma - x * np.cos(angle)) / np.sin(angle)
+
+
+"""
+Plan:
+1) Try to at least generate a conway worm
+"""
+
+
+xspace = np.linspace(-5, 5)
+rng = np.random.default_rng(32187)
+
+K_RANGE = 3
 J_RANGE = 5
-K_RANGE = 1
-SIGMA = 0.5
 
-    
+sigmas = []
 
-def draw_construction_line(xspace, j, k, sigma):
-    """
-    Function of the k'th grid line of set j, with normal offset sigma. 
-    """
-    angle = j * 2 * np.pi/5
-    s = np.sin(angle)
-    c = np.cos(angle)
-    # Special cases. E.g angle = 0 or 90 degrees
-    if c == 0:
-        print("Vertical")
-        plt.axhline(y=( (k - sigma)/s ))
-    elif s == 0:
-        yval = (k - sigma)/s
-        plt.plot(xspace, [yval for i in range(len(xspace))])
-    else:
-        plt.plot(xspace, (k - sigma - xspace * c)/s)
+js = [j+0.25 for j in range(J_RANGE)]
 
-
-def find_crossing(j1, j2, k1, k2, sigmaj1, sigmaj2):
-    """
-    Finds the crossing points between two of the lines given.
-    Lines of different sets *will* cross once because they are each infinite lines, and each line
-    is at some angle of a circle. No two lines of *different* sets will be parallel.
-    """
-    a1 = j1 * 2 * np.pi/5
-    a2 = j2 * 2 * np.pi/5
-    # y = ( np.cos(a2) * (k1 - sigmaj1) - np.cos(a1) * (k2 - sigmaj2) )/np.sin( a2 - a1 )
-    # x = (k2 - sigmaj2 - y * np.sin(a2))/np.cos(a2)
-    mu1 = k1 - sigmaj1
-    mu2 = k2 - sigmaj2
-    c1 = np.cos(a1)
-    c2 = np.cos(a2)
-    s1 = np.sin(a1)
-    s2 = np.sin(a2)
-
-    x = mu1/c1 + mu2/c2 - (mu1 * s2)/(s1 * c2) - (mu2 * s1)/(s2 * c1)
-    y = mu1 - x * c1/s1
-    return x, y
-
-xspace = np.linspace(-11, 11)
-
-for j in range(J_RANGE):
+for j in js:
+    offset = 1 + rng.random() * 2
+    sigmas.append(offset)
     for k in range(K_RANGE):
-        draw_construction_line(xspace, j, k, SIGMA)
+        plt.plot(xspace, construction_line(xspace, j, k, offset))
 
-# Find where the lines cross. Go through each set and compare against each other line in each other set.
-# There is probably a much better way to do this but this will do for now as a concept
-x_crossings = []
-y_crossings = []
 
-for j1 in range(J_RANGE): # Compare each set with each other set
-    for j2 in range(j1+1, J_RANGE):   # If at j1 = 3, we would have already compared 3-0, 3-1, 3-2.
-        for k1 in range(K_RANGE):
-            for k2 in range(K_RANGE):
-                # Compare k1 with k2 of set j1 and j2 respectively. 
-                x, y = find_crossing(j1, j2, k1, k2, SIGMA, SIGMA)
-                print("Crossing found:", x, y)
-                x_crossings.append(x)
-                y_crossings.append(y)
-
-print("Number of crossings found:", len(x_crossings))
-plt.plot(x_crossings, y_crossings, "x")
+# Just find intersections along one line for now
+# Let's choose j1 = 0, k1 = 0 and compare that with every other line
+for j2 in js[1:]:   # Every set apart from set 0
+    for k2 in range(K_RANGE):   # Go through each line of the set j2
+        intersection = Intersection()
+    
 
 plt.show()
