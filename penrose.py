0b6fda2a4119f1e4ce923a22072854374fecb7bd
Josh Colclough <colcloughj2@cardiff.ac.uk>
2021-10-17 15:40:43 +0100

    Finds indices surrounding each intersection

--- penrose.py
+++ penrose.py
@@ -10,14 +10,13 @@ def construction_line(x, j, k, sigma, symmetry=5, angle_offset=ANGLE_OFFSET):
     return (k - sigma - x * np.cos(angle)) / np.sin(angle)
 
 
-def get_indices(r, sigmas, symmetry=5, angle_offset=ANGLE_OFFSET):
+def get_indices(r, sigmas, es, symmetry=5, angle_offset=ANGLE_OFFSET):
     """
     Returns the indices for any point on the plane.
     [a j0, b j1, c j2, d j3, e j4] where a,b,c,d,e are integers.
+    `es` are the normal vectors for each line.
     """
-    # Define normal unit vectors for each of the sets. Normal to the lines so: angle + pi/2.
-    es = [np.array([ np.cos( (j * 2 * np.pi/symmetry) + angle_offset + np.pi/2.0 ), np.sin( (j * 2 * np.pi/symmetry) + angle_offset + np.pi/2 ) ]) for j in range(symmetry)]
-    print(es)
+    
     # Dot product with the unit vector will return the index without the original sigma shift, so then add the shift.
     # Note that since k is an integer, the distance between the lines is just 1 so it is already normalised.
     indices = np.zeros(5, dtype=int)
@@ -68,14 +67,21 @@ class Intersection:
         of the two lines that intersected.
         """
         # Get indices that the point is located at
-        point_indices = get_indices(self.r, symmetry, angle_offset)
+        point_indices = get_indices(self.r, sigmas, symmetry, angle_offset)
         point_indices[self.j1] = self.k1  # These are already known
         point_indices[self.j2] = self.k2
         # The indices of the spaces surrounding the points are then (for j1 = 0, j2 = 1):
         # [k1 + 1 or 0, k2 + 1 or 0, point_indices...]
         # There should be 4 total
-        surrounding_indices = [np.zeros(5, dtype=int) for i in range(4)]
-        surrounding_indices[]
+        # NOTE: DOESN'T WORK WITH DIFFERENT SYMMETRY YET
+        # had to copy each member of point_indices due to it just putting the array in by reference each time
+        surrounding_indices = [[point_indices[j] for j in range(len(point_indices))] for i in range(4)]
+        # Do each permutation
+        surrounding_indices[1][self.j1] += 1
+        surrounding_indices[2][self.j2] += 1
+        surrounding_indices[3][self.j1] += 1
+        surrounding_indices[3][self.j2] += 1
+        return surrounding_indices
 
 """
 Plan:
@@ -84,6 +90,8 @@ Plan:
 3) Use those indices in de Bruijn's formula to find the vertices in 2D space.
 """
 
+# Define normal unit vectors for each of the sets. Normal to the lines so: angle + pi/2. Required for finding indices
+es = [np.array([ np.cos( (j * 2 * np.pi/5) + ANGLE_OFFSET + np.pi/2.0 ), np.sin( (j * 2 * np.pi/5) + ANGLE_OFFSET + np.pi/2 ) ]) for j in range(5)]
 
 xspace = np.linspace(-25, 25)
 rng = np.random.default_rng(32187)
@@ -91,15 +99,16 @@ rng = np.random.default_rng(32187)
 K_RANGE = 10
 J_RANGE = 5
 
-sigmas = []
+sigmas = np.zeros(5)
 
 for j in range(J_RANGE):
     offset = 1 + rng.random() * 2
-    sigmas.append(offset)
-    for k in range(K_RANGE):
-        plt.plot(xspace, construction_line(xspace, j, k, offset), color=["r", "g", "b", "m", "y"][j])
+    sigmas[j] = offset
+    # for k in range(K_RANGE):
+    #     plt.plot(xspace, construction_line(xspace, j, k, offset), color=["r", "g", "b", "m", "y"][j])
 
 plt.legend()
+
 # Just find intersections along one line for now
 # Let's choose j1 = 1, k1 = 1 and compare that with every other line
 # Haven't drawn set 0 properly yet so set 1 is fine
@@ -109,8 +118,7 @@ intersections = []
 
 
 for j1 in range(J_RANGE):
-    for j2 in range(j1 + 1, J_RANGE):
-        print("Comparing", j1, j2)
+    for j2 in range(j1 + 1, J_RANGE):   # Compares 0 1, 0 2, 0 3, 0 4, 1 2, 1 3, ... 3 4
         for k1 in range(K_RANGE):
             for k2 in range(K_RANGE):   # Go through each line of the set j2
                 intersection = Intersection(j1, k1, j2, k2, sigmas[j1], sigmas[j2])
@@ -118,10 +126,11 @@ for j1 in range(J_RANGE):
                 x_intersections.append(intersection.r[0])
                 y_intersections.append(intersection.r[1])
 
-plt.plot(x_intersections, y_intersections, "xr")
+# plt.plot(x_intersections, y_intersections, "xr")
+
+print("Intersections:", intersections[0].find_surrounding_indices(sigmas, es))
+indices = [i.find_surrounding_indices(sigmas, es) for i in intersections]
+print(indices)
 
 
-print("ind:", get_indices( np.array([1, 0.0]), sigmas ))
-
-plt.show()
 
